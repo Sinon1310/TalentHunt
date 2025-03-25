02c290e2801a192ea8421bdaf1f4a138a0e98b86
nickPereira-1307 <nickpereira13072005@gmail.com>
2025-03-25 13:41:34 +0530

    users schema made and connection made

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,14 @@ const mongoose = require("mongoose");
 const cors = require("cors");
 require("dotenv").config();
 
+
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 const PORT = process.env.PORT || 5002;
 
-// Connect to MongoDB Atlas
+ //Connect to MongoDB Atlas
 //mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     //.then(() => console.log("MongoDB Connected"))
    // .catch(err => console.log(err));
